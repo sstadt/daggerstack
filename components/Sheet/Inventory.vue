15cb169145aef0957e64570b3e659d6a53421b70
Scott Stadt <scott.stadt@gmail.com>
2024-03-21 16:34:06 -0400

    tinkering

--- components/Sheet/Inventory.vue
+++ components/Sheet/Inventory.vue
@@ -3,10 +3,10 @@
     InputTextarea.pb-8.border-b.mb-8.mt-6(label="Items")
     .pb-8.border-b.mb-8
       h3.text-xl.font-black.uppercase.mb-4 Inventory Weapon
-      .flex.mb-4
-        InputText(class="w-5/12" label="name" inline)
-        InputText(class="w-1/4" label="trait & range" inline)
-        InputText(class="w-1/3" label="damage dice")
+      InputText(label="name")
+      .flex
+        InputText(class="w-1/2" label="trait & range" inline)
+        InputText(class="w-1/2" label="damage dice")
       InputText(label="feature")
     h3.text-xl.font-black.uppercase.mb-4 Inventory Armor
     .flex.mb-4
