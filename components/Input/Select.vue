3840997d52ff89fe14081c0e9af10526023b8bd2
Scott Stadt <scott.stadt@gmail.com>
2024-03-27 18:03:31 -0400

    additional tweaks to select

--- components/Input/Select.vue
+++ components/Input/Select.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .input--select.flex.flex-col-reverse(:class="wrapperClasses")
     label.uppercase.font-bold.text-slate-500(v-if="label && !hideLabel") {{ label }}
-    select.w-full.px-4.py-2.text-xl.bg-slate-100(
+    select.w-full.px-4.py-2.text-xl.bg-slate-100.outline-0(
       class="focus:bg-slate-200"
     )
       option(v-if="!required")
--- components/Trait/Select.vue
+++ components/Trait/Select.vue
@@ -3,10 +3,10 @@
     .shadow.border.border-slate-400.border-solid.rounded-md.flex.flex-col.items-center
       .bg-black.px-2.rounded-t.flex.justify-center.items-center.space-x-1.w-full(class="py-0.5")
         label.text-xs.uppercase.font-bold.text-white.truncate(v-if="label") {{ label }}
-      select.text-3xl.my-auto.py-4.px-2.text-center(
+      select.text-3xl.my-auto.py-4.px-2.w-full.rounded.text-center.outline-0(
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
-        class="w-5/6"
+        class="w-5/6 focus:bg-slate-300"
       )
         option
         option(v-for="option in options" :value="option.value") {{ option.label }}
