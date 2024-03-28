85e9bd1396621cbb318c511d5e1f68f2f4c96d6e
Scott Stadt <scott.stadt@gmail.com>
2024-03-28 17:21:32 -0400

    added remaining class data

--- data/classes.js
+++ data/classes.js
@@ -1,5 +1,21 @@
+import bard from './classes/bard.json';
+import druid from './classes/druid.json';
 import guardian from './classes/guardian.json';
+import ranger from './classes/ranger.json';
+import rogue from './classes/rogue.json';
+import seraph from './classes/seraph.json';
+import sorcerer from './classes/sorcerer.json';
+import warrior from './classes/warrior.json';
+import wizard from './classes/wizard.json';
 
 export default {
+  bard,
+  druid,
   guardian,
+  ranger,
+  rogue,
+  seraph,
+  sorcerer,
+  warrior,
+  wizard,
 };
