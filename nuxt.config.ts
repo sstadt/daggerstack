1d6f889d1ff2a92015112adbf09744f16d503c18
Scott Stadt <scott.stadt@gmail.com>
2024-08-16 18:13:17 -0400

    switched gtag implementation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpZnRuZW9jdHBxZmVmeGdlbGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkwNzAzMzAsImV4cCI6MjAzNDY0NjMzMH0.w1kyOcbuQodwHb7tncNbJaLEGPLRs3rwELcYWQH1cq0',
   },
   plugins: [
+    '~/plugins/gtag',
     '~/plugins/pinia-debounce',
     '~/plugins/vue3-touch-events',
     '~/plugins/vue3-mq',
@@ -26,22 +27,6 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/supabase',
-    [
-      'nuxt-gtag',
-      {
-        id: 'G-ZMR94R58WX',
-        initCommands: [
-          // Setup up consent mode
-          ['consent', 'default', {
-            ad_user_data: 'denied',
-            ad_personalization: 'denied',
-            ad_storage: 'denied',
-            analytics_storage: 'denied',
-            wait_for_update: 500,
-          }]
-        ],
-      },
-    ],
     [
       '@nuxtjs/google-fonts',
       {
